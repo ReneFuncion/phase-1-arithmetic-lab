849f42965c835f81010f8dd18e78deff36f9d83a
Rene Funcion <rcfun959@gmail.com>
2023-03-15 17:26:14 -0700

    Completed phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+const max = Math.max(2, 5, 19, 20, 19.999999, -400);
+let num1 = 2.45333;
+let num2 = 31;
+const multiply = Math.floor(num1) * Math.floor(num2);
+let num3 = 39.987, num4 = 5.0;
+const mod = Math.floor(num3) % Math.floor(num4);
